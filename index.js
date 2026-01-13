b363a5648bcdfbee153304b761e5d7a20057a649
tj-commits <thefoxjones@gmail.com>
2026-01-13 16:18:10 -0600

    remoev lolite

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const booleans = require("lolite/src/private/arrayOfAllBooleans")
+const { call } = require("basic-functions")
+const map = require("array-map")
+let booleans = ["true-value", "false-value"]
+booleans = map(booleans, require)
+booleans = map(booleans, call)
+booleans = map(booleans, require("@f/identity"))
 const sample = require("lodash.sample")
 
 function randomBoolean() {
